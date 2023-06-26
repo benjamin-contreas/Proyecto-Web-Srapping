2b503389d5ed23bf21a1353e362fdcd1d0e7719b
benjamin-contreas <becontreras@alumnos.uai.cl>
2023-06-25 20:36:30 -0400

    Update 1
    Se combinaron exitosamente los dos códigos

--- proyecto_final.py
+++ proyecto_final.py
@@ -5,6 +5,7 @@ from selenium import webdriver                          # Para realizar web scra
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import csv                                              # Para exportar a CSV
 import uf
 import producto
 
@@ -87,6 +88,30 @@ def printProducto(producto):
     print(producto.descripcion)
     print(producto.precioPesos)
     print(producto.precioUf)
+
+def menorPrecio(sPrices):
+    # Find all the price elements within sPrices
+    precio_tarjeta = sPrices.find_all('li', class_='catalog-prices__card-price')
+    precio_oferta = sPrices.find_all('li', class_='catalog-prices__offer-price')
+    precio_normal = sPrices.find_all('li', class_='catalog-prices__list-price')
+
+    # Check if any price elements are found and extract the lowest price
+    if precio_tarjeta:
+        return int(precio_tarjeta[0].text.replace('$', '').replace('.', ''))
+    elif precio_oferta:
+        return int(precio_oferta[0].text.replace('$', '').replace('.', ''))
+    elif precio_normal:
+        return int(precio_normal[0].text.replace('$', '').replace('.', ''))
+    else:
+        return None  # Return None if no prices are found
+    
+def resultadosCsv(listaResultados, nombresColumnas, filename):
+  with open(filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(nombresColumnas)
+    for objeto in listaResultados:
+      fila = [getattr(objeto, columna) for columna in nombresColumnas]
+      writer.writerow(fila)
 
 
 if (__name__ == '__main__'):
@@ -321,6 +346,133 @@ if (__name__ == '__main__'):
     
     # Proceso finalizado
     if (B_VERBOSE_DEBUG):
-        print('Proceso finalizado')
+        print('Proceso Falabella finalizado')
+
+    # 2.2 Búsqueda en Ripley
+    for S_FIND in patronesBusqueda:
+
+        if (B_VERBOSE_DEBUG):
+            print('=' * len('Patrón de búsqueda: {}'.format(S_FIND)))
+            print('Patrón de búsqueda: {}'.format(S_FIND))
+            print('=' * len('Patrón de búsqueda: {}'.format(S_FIND)))
+        
+        # Driver y carga de página
+        driver = iniciarDriver()
+        driver.get('https://simple.ripley.cl/')
+        mySleep(2)
+
+        # Ingresar producto en la barra de búsqueda
+        inputText = driver.find_element(By.XPATH, '/html/body/div[5]/header/section/nav/ul/li[1]/div/div[1]/input')
+        inputText.send_keys(S_FIND)
+        inputText.send_keys(Keys.ENTER)
+        mySleep(3)
+
+        # Verificar si hay datos
+        bOkExistData = False
+        try:
+            sXpath = '/html/body/div[9]/div[2]/div/div[2]/div[3]/section/div/div' 
+            btnPage1 = driver.find_element(By.XPATH, sXpath)
+            bOkExistData = True
+            print('Hay datos')
+        except: 
+            print('No hay datos')
+            pass
+
+        if (bOkExistData):
+            try:
+                mySleep(2)
+                sXpath = '/html/body/div[9]/div[2]/div/div[2]/div[3]/section/div/div' 
+                contentData = driver.find_element(By.XPATH, sXpath)
+                htmlData = contentData.get_attribute('innerHTML')
+                lxmlData = BeautifulSoup(htmlData, 'lxml')
+                outputHtml('ripley.html', lxmlData)
+            except:
+                print("No hay mas paginas")
+
+        # Iterar en todas las páginas
+        nPage = 1
+        while (bOkExistData):
+            if (B_VERBOSE_DEBUG):
+                print('{}: Página {}'.format(S_FIND, nPage))
+
+            # Capturar datos desde el contenedor
+            try:
+                # Esperamos a que termine de cargar 
+                # Luego de carga inicial para primera pasada
+                # O luego de páginación para siguientes pasadas
+                sXpath = '/html/body/div[9]/div[2]/div/div[2]/div[3]/section/div/div'
+                mySleepUntilObject(20, driver, sXpath)
+                mySleep(4)
+
+                # Capturamos HTML del contenedor de productos tecnológicos
+                sXpath = '/html/body/div[9]/div[2]/div/div[2]/div[3]/section/div/div'
+                contentData = driver.find_element(By.XPATH, sXpath)
+                htmlData = contentData.get_attribute('innerHTML')
+                lxmlData = BeautifulSoup(htmlData, 'lxml')
+
+                # Capturamos datos del contenedor
+                sNames = lxmlData.find_all('div', class_= 'catalog-product-details__name')
+                sPrices = lxmlData.find_all('div', class_ = 'catalog-product-details__prices')
+                # Recorremos el contenedor para llenar lista
+                for i in range(len(sNames)):
+                    nPrecio = menorPrecio(sPrices[i])
+                    print(nPrecio)
+                    precioUF = float(nPrecio) / ufHoy.precio
+                    miProducto = producto.Producto(S_FIND, "Ripley" ,sNames[i].string, nPrecio, precioUF)
+                    listResult.append(miProducto)                    
+                    # Imprimimos
+                    if (B_VERBOSE_DEBUG):
+                        printProducto(miProducto)
+                
+                # Dar click a la siguiente página
+                # Obtenemos botón próxima página, sino se caerá y será capturado en except
+                if (S_FIND == 'notebook hp'):
+                    link_element = driver.find_element(By.XPATH, '/html/body/div[9]/div[2]/div/div[2]/div[4]/nav/ul/li[5]/a')
+                    link_url = link_element.get_attribute('href')
+                    # Aplicar esta solución al resto de los casos.
+                    if link_url == driver.current_url + "#":
+                        print('No hay más páginas')
+                        bOkExistData = False      
+                    driver.get(link_url)              
+                if (S_FIND == 'impresora 3d'):
+                    link_element = driver.find_element(By.XPATH,'/html/body/div[9]/div[2]/div/div[2]/div[4]/nav/ul/li[13]/a')
+                    link_url = link_element.get_attribute('href')
+                    if link_url == driver.current_url + "#":
+                        print('No hay más páginas')
+                        bOkExistData = False
+                    driver.get(link_url)
+                if (S_FIND == 'tablet samsung'):
+                    link_element = driver.find_element(By.XPATH,'/html/body/div[9]/div[2]/div/div[2]/div[4]/nav/ul/li[3]/a')
+                    link_url = link_element.get_attribute('href')
+                    if link_url == driver.current_url + "#":
+                        print('No hay más páginas')
+                        bOkExistData = False                                     
+            except:
+                if (B_VERBOSE_DEBUG):
+                    print('Caída al capturar contenedor')
+                    #print('ClassError: {} - NameError: {}'.format(sys.exc_info()[0], sys.exc_info()[1]))
+                bOkExistData = False
+
+            # Incrementamos página
+            nPage = nPage + 1
+
+    # Cierre del driver
+    driver.close()
+    driver.quit()
+
+    # Imprimir capturas de datos
+    if (B_VERBOSE_RESULT):
+        print('=' * len('Lista total:'))
+        print('Lista total:')       
+        print('=' * len('Lista total:'))
+        #print(*listResult, sep='\n')
+        [print('"{}";"{}";{}'.format(item.patronBusqueda, item.descripcion, item.precioPesos)) for item in listResult]
+    
+    # Proceso finalizado
+    if (B_VERBOSE_DEBUG):
+        print('Proceso Ripley finalizado')
+
+    # 3: Exportar a CSV
+    resultadosCsv(listResult, ['patronBusqueda', 'multitienda', 'descripcion', 'precioPesos', 'precioUf'], 'todosolo.csv')
 
     
