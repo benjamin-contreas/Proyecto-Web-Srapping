0b92112dc516de932d03916bfb4c3087b3bd84ce
benjamin-contreas <becontreras@alumnos.uai.cl>
2023-06-26 12:18:58 -0400

    Update proyecto_final.py

--- proyecto_final.py
+++ proyecto_final.py
@@ -5,9 +5,13 @@ from selenium import webdriver                          # Para realizar web scra
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from itertools import groupby
+from operator import itemgetter
+from itertools import product
 import csv                                              # Para exportar a CSV
 import uf
 import producto
+
 
 # Constantes
 B_VERBOSE_DEBUG = True                                  # Para debug
@@ -69,6 +73,7 @@ def mySleep(nTimeOut):
     while (nTimeDifference < nTimeOut):
         nTimeDifference = time.time() - nTimeInit
 
+# Leer archivo de patrones de búsqueda
 def listaBusqueda(fname):
     file_path = fname
     file = open(file_path, "r")
@@ -82,6 +87,7 @@ def listaBusqueda(fname):
     print(elements_list)
     return elements_list
 
+# Imprimir atributos de un objeto Producto
 def printProducto(producto):
     print(producto.patronBusqueda)
     print(producto.multitienda)
@@ -89,13 +95,13 @@ def printProducto(producto):
     print(producto.precioPesos)
     print(producto.precioUf)
 
+# Obtener el menor precio de un contenedor de precios de Ripley
 def menorPrecio(sPrices):
-    # Find all the price elements within sPrices
     precio_tarjeta = sPrices.find_all('li', class_='catalog-prices__card-price')
     precio_oferta = sPrices.find_all('li', class_='catalog-prices__offer-price')
     precio_normal = sPrices.find_all('li', class_='catalog-prices__list-price')
 
-    # Check if any price elements are found and extract the lowest price
+    # Revisamos si existe el precio en tarjeta, oferta o normal
     if precio_tarjeta:
         return int(precio_tarjeta[0].text.replace('$', '').replace('.', ''))
     elif precio_oferta:
@@ -103,8 +109,9 @@ def menorPrecio(sPrices):
     elif precio_normal:
         return int(precio_normal[0].text.replace('$', '').replace('.', ''))
     else:
-        return None  # Return None if no prices are found
+        return None
     
+# Exportar a CSV
 def resultadosCsv(listaResultados, nombresColumnas, filename):
   with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
@@ -113,6 +120,201 @@ def resultadosCsv(listaResultados, nombresColumnas, filename):
       fila = [getattr(objeto, columna) for columna in nombresColumnas]
       writer.writerow(fila)
 
+# Parsear HTML con lxml
+def lxmlParse(sXpath):
+    contentData = driver.find_element(By.XPATH, sXpath)
+    htmlData = contentData.get_attribute('innerHTML')
+    lxmlData = BeautifulSoup(htmlData, 'lxml')
+    return lxmlData
+
+#Función para leer csv uando open csv
+def lectura_csv(archivo_csv):
+  resultado = []
+  with open(archivo_csv, "r") as archivo_csv:
+    lector = csv.reader(archivo_csv, delimiter = ";")
+    resultado = list(lector)
+  return resultado
+todosolo = lectura_csv("todosolo.csv")
+
+#----------Funciones-------Auxiliares-----------Para---------PArte-------------4
+#Función para tener lista de tiendas unicos
+def tiendas_unicos(todosolo):
+  tiendas = set(multitiendas[1] for multitiendas in todosolo[1:])#Selecciono solo lo importante, los titulos no
+  return list(tiendas) #Función list nos entrega datos no repetidos 
+
+#Función para tener lista de patrones unicos
+def patrones_unicos(todosolo):
+  patrones = set(patron[0] for patron in todosolo[1:])#Selecciono solo lo importante, los titulos no
+  return list(patrones) #Función list nos entrega datos no repetidos 
+
+#Función para obtener los precios 
+#Minimo_Patron_tienda
+def precioMinimo_Patron_Multitienda(data,patron,tienda):
+  #Lista con todos los precios por combinación seleccionada
+  preciosUF = (float(precios[4].replace(",",".")) for precios in data[1:] if precios[0] == patron and precios[1] == tienda)
+  #Extraemos el minimo
+  PMin = min(preciosUF)
+  #Extraemos el presio minimo en pesos y UF
+  precios_pesos_uf = ((precios[3],precios[4]) for precios in data[1:] if float(precios[4].replace(",",".")) == PMin)
+  precios = list(precios_pesos_uf)
+  return precios
+
+#Maximo_PAtron_tienda
+def precioMaximo_Patron_Multitienda(data,patron,tienda):
+  #Lo mismo que el anterior pero con el maximo
+  preciosUF = (float(precios[4].replace(",",".")) for precios in data[1:] if precios[0] == patron and precios[1] == tienda)
+  PMax = max(preciosUF)
+  precios_pesos_uf = ((precios[3],precios[4]) for precios in data[1:] if float(precios[4].replace(",",".")) == PMax)
+  precios = list(precios_pesos_uf)
+  return precios
+
+#Minimo_Patron
+def precioMinimo_Patron(data,patron):
+  #Lista con todos los precios por combinación seleccionada
+  preciosUF = (float(precios[4].replace(",",".")) for precios in data[1:] if precios[0] == patron)
+  #Extraemos el minimo
+  PMin = min(preciosUF)
+  #Extraemos el presio minimo en pesos y UF
+  precios_pesos_uf = ((precios[3],precios[4]) for precios in data[1:] if float(precios[4].replace(",",".")) == PMin)
+  precio = list(precios_pesos_uf)
+  return precio
+
+#Maximo_Patron
+def precioMaximo_Patron(data,patron):
+  #Lista con todos los precios por combinación seleccionada
+  preciosUF = (float(precios[4].replace(",",".")) for precios in data[1:] if precios[0] == patron)
+  #Extraemos el maximo
+  PMax = max(preciosUF)
+  #Extraemos el presio maximo en pesos y UF
+  precios_pesos_uf = ((precios[3],precios[4]) for precios in data[1:] if float(precios[4].replace(",",".")) == PMax)
+  precios = list(precios_pesos_uf)
+  return precios
+
+
+#Promedio PATRON Y MULTITIENDA
+#UF
+def Promedio_Patron_Multitienda_UF(data,patron,tienda):
+  precio = (float(precios[4].replace(",",".")) for precios in data[1:] if precios[0] == patron and precios[1] == tienda)
+  precios = list(precio)
+  suma = sum(precios)
+  largo_lista = len(precios)
+  promedio = suma/largo_lista
+  return promedio
+
+#Pesos
+def Promedio_Patron_Multitienda_pesos(data,patron,tienda):
+  precio = (float(precios[3].replace(",",".")) for precios in data[1:] if precios[0] == patron and precios[1] == tienda)
+  precios = list(precio)
+  suma = sum(precios)
+  largo_lista = len(precios)
+  promedio = suma/largo_lista
+  return promedio
+
+#Promedio PATRON
+#UF
+def Promedio_Patron_UF(data,patron):
+  precio = (float(precios[4].replace(",",".")) for precios in data[1:] if precios[0] == patron)
+  precios = list(precio)
+  suma = sum(precios)
+  largo_lista = len(precios)
+  promedio = suma/largo_lista
+  return promedio
+
+#Pesos
+def Promedio_Patron_pesos(data,patron):
+  precio = (float(precios[3].replace(",",".")) for precios in data[1:] if precios[0] == patron)
+  precios = list(precio)
+  suma = sum(precios)
+  largo_lista = len(precios)
+  promedio = suma/largo_lista
+  return promedio
+
+#Combinaciones
+def combinaciones_patrones_tiendas(patrones,tiendas):
+  Comb_Pat_Tiend = list(product(patrones,tiendas))
+  return Comb_Pat_Tiend
+#---------------------------FUNCIONES-----PEDIDAS-----EN--------PARTE------------4
+#PRECIOS MINIMOS POR PATRON DE BUSQUEDA  Y MULTITIENDA
+def Precios_MIN_PATRON_MULT(todosolo):
+  #Sacamos todas las combinaciones
+  combinaciones=combinaciones_patrones_tiendas(patrones_unicos(todosolo),tiendas_unicos(todosolo))
+  #Obtenemos los minimos de las combinaciones
+  Precios_minimos = ((fila[0],fila[1],precioMinimo_Patron_Multitienda(todosolo,fila[0],fila[1])[0][0],precioMinimo_Patron_Multitienda(todosolo,fila[0],fila[1])[0][1]) for fila in combinaciones)
+  #Lo convertimos en una lista
+  lista_precios_minimos = list(Precios_minimos)
+  return lista_precios_minimos
+
+#PRECIOS MAXIMOS POR PATRON DE BUSQUEDA  Y MULTITIENDA
+def Precios_MAX_PATRON_MULT(todosolo):
+  #Este codigo hace lo mismo que el anterior pero para los maximos precios en UF
+  combinaciones=combinaciones_patrones_tiendas(patrones_unicos(todosolo),tiendas_unicos(todosolo))
+  Precios_maximos = ((fila[0],fila[1],precioMaximo_Patron_Multitienda(todosolo,fila[0],fila[1])[0][0],precioMaximo_Patron_Multitienda(todosolo,fila[0],fila[1])[0][1]) for fila in combinaciones)
+  lista_precios_maximos = list(Precios_maximos)
+  return lista_precios_maximos
+
+#PRECIOS PROMEDIO POR PATRON DE BUSQUEDA Y MULTITIENDA
+def PRECIOS_PROMEDIO_PESOS_UF(todosolo):
+  #Este codigo hace lo mismo que el anterior pero para los maximos precios en UF
+  combinaciones=combinaciones_patrones_tiendas(patrones_unicos(todosolo),tiendas_unicos(todosolo))
+  Precios_promedios = ((fila[0],fila[1],Promedio_Patron_Multitienda_pesos(todosolo,fila[0],fila[1]),Promedio_Patron_Multitienda_UF(todosolo,fila[0],fila[1])) for fila in combinaciones)
+  precios_promedios = list(Precios_promedios)
+  return precios_promedios
+
+#PRECIOS MINIMOS POR PATRON DE BUSQUEDA
+def Precios_MIN_PATRON(todosolo):
+  #Sacamos todos los patrones
+  patrones=patrones_unicos(todosolo)
+  #Obtenemos los minimos de las combinaciones
+  Precios_minimos = ((fila,precioMinimo_Patron(todosolo,fila)[0][0],precioMinimo_Patron(todosolo,fila)[0][1]) for fila in patrones)
+  #Lo convertimos en una lista
+  lista_precios_minimos = list(Precios_minimos)
+  return lista_precios_minimos
+
+#PRECIOS MAXIMOS POR PATRON DE BUSQUEDA
+def Precios_MAX_PATRON(todosolo):
+  #Sacamos todos los patrones
+  patrones=patrones_unicos(todosolo)
+  #Obtenemos los minimos de las combinaciones
+  Precios_maximos = ((fila,precioMaximo_Patron(todosolo,fila)[0][0],precioMaximo_Patron(todosolo,fila)[0][1]) for fila in patrones)
+  #Lo convertimos en una lista
+  lista_precios_maximos = list(Precios_maximos)
+  return lista_precios_maximos
+
+#PRECIOS PROMEDIO POR PATRON DE BUSQUEDA
+def PRECIOS_PROMEDIO_PATRONES_PESOS_UF(todosolo):
+  #Este codigo hace lo mismo que el anterior pero para los maximos precios en UF
+  patrones=patrones_unicos(todosolo)
+  Precios_promedios = ((fila,Promedio_Patron_pesos(todosolo,fila),Promedio_Patron_UF(todosolo,fila)) for fila in patrones)
+  precios_promedios = list(Precios_promedios)
+  return precios_promedios
+
+#Minimo_bulto
+def precioMinimo_bulto(data):
+  #Lista con todos los precios por combinación seleccionada
+  preciosUF = (float(precios[4].replace(",",".")) for precios in data[1:])
+  #Extraemos el minimo
+  PMin = min(preciosUF)
+  #Extraemos el presio minimo en pesos y UF
+  precios_pesos_uf = (precios for precios in data[1:] if float(precios[4].replace(",",".")) == PMin)
+  precio = list(precios_pesos_uf)
+  return precio
+
+#Maximo_bulto
+def precioMaximo_bulto(data):
+  #Lista con todos los precios por combinación seleccionada
+  preciosUF = (float(precios[4].replace(",",".")) for precios in data[1:])
+  #Extraemos el maximo
+  PMax = max(preciosUF)
+  #Extraemos el presio maximo en pesos y UF
+  precios_pesos_uf = (precios for precios in data[1:] if float(precios[4].replace(",",".")) == PMax)
+  precios = list(precios_pesos_uf)
+  return precios
+
+#Función para imprimir las listas
+def print_lista(lista):
+  for file in lista:
+    print(file)
+  print("")
 
 if (__name__ == '__main__'):
 
@@ -121,9 +323,7 @@ if (__name__ == '__main__'):
     driver = iniciarDriver()
     driver.get('https://www.bcentral.cl')
 
-    weUFContent = driver.find_element(By.XPATH, '/html/body/div[1]/section/div/div[2]/div/div/div[1]/section/div/div[2]/div/div/div/div/div[1]/div/div')
-    htmlData = weUFContent.get_attribute('innerHTML')
-    lxmlData = BeautifulSoup(htmlData, 'lxml')
+    lxmlData = lxmlParse('/html/body/div[1]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div[1]')
 
     sTipoCambio= lxmlData.find_all("p", class_= "basic-text fs-2 f-opensans-bold text-center c-blue-nb-2")[0].text
     ufHoy = uf.UF(sTipoCambio, time.strftime("%d/%m/%Y"))
@@ -163,6 +363,7 @@ if (__name__ == '__main__'):
             print('No hay ventana emergente')
             pass
 
+        # Ingresar producto en la barra de búsqueda
         inputText = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/div/input')
         inputText.send_keys(S_FIND)
         inputText.send_keys(Keys.ENTER)
@@ -176,6 +377,7 @@ if (__name__ == '__main__'):
             bOkExistData = True
             print('Hay datos')
         except: 
+            # Si no hay datos, intentamos con otro XPATH
             try:
                 sXpath = '/html/body/div[1]/div/div/div[2]/section[2]/div/div[3]' 
                 btnPage1 = driver.find_element(By.XPATH, sXpath)
@@ -188,14 +390,14 @@ if (__name__ == '__main__'):
         
         if (bOkExistData):
             try:
+                # Capturamos HTML del contenedor de productos tecnológicos
                 mySleep(2)
                 if (S_FIND == 'impresora 3d'):
                     sXpath = '/html/body/div[1]/div/div/div[2]/section[2]/div/div[3]' 
                 else:
                     sXpath = '/html/body/div[1]/div/div[2]/div[2]/section[2]/div/div[3]'
-                contentData = driver.find_element(By.XPATH, sXpath)
-                htmlData = contentData.get_attribute('innerHTML')
-                lxmlData = BeautifulSoup(htmlData, 'lxml')
+                
+                lmxlData = lxmlParse(sXpath)
                 outputHtml('falabella.html', lxmlData)
             
             except:
@@ -475,4 +677,38 @@ if (__name__ == '__main__'):
     # 3: Exportar a CSV
     resultadosCsv(listResult, ['patronBusqueda', 'multitienda', 'descripcion', 'precioPesos', 'precioUf'], 'todosolo.csv')
 
+    # 4: Obtener los precios mínimos, máximos y promedios por patrón de búsqueda y multitienda
+    Minimos_PAT_MULT =Precios_MIN_PATRON_MULT(todosolo)
+    Maximos_PAT_MULT =Precios_MAX_PATRON_MULT(todosolo)
+    Promedios_PAT_MULT = PRECIOS_PROMEDIO_PESOS_UF(todosolo)
+    Minimos_PAT = Precios_MIN_PATRON(todosolo)
+    Maximos_PAT = Precios_MAX_PATRON(todosolo)
+    Promedios_PAT = PRECIOS_PROMEDIO_PATRONES_PESOS_UF(todosolo)
+    Minimo_Bulto = precioMinimo_bulto(todosolo)
+    Maximo_Bulto = precioMaximo_bulto(todosolo)
+
+
+    print("PRECIOS MINIMOS POR PATRON Y BUSQUEDA")
+    print_lista(Minimos_PAT_MULT)
     
+    print("PRECIOS MAXIMOS POR PATRON Y BUSQUEDA")
+    print_lista(Maximos_PAT_MULT)
+
+    print("PROMEDIO DE PRECIOS POR PATRON Y BUSQUEDA EN UF Y PESOS") 
+    print_lista(Promedios_PAT_MULT)
+
+    print("PRECIOS MINIMOS POR PATRON")
+    print_lista(Minimos_PAT)
+
+    print("PRECIOS MAXIMOS POR PATRON")
+    print_lista(Maximos_PAT)
+
+    print("PROMEDIO DE PRECIOS POR PATRON EN UF Y PESOS") 
+    print_lista(Promedios_PAT)
+
+    print("PRECIOS MINIMOS BULTO")
+    print_lista(Minimo_Bulto)
+
+    print("PRECIOS MAXIMOS BULTO")
+    print_lista(Maximo_Bulto)
+        
